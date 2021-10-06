176867ef6cbe63ed7983433fc6262879dfffbc80
trundle.moses <trundle.moses@gmail.com>
2021-10-06 10:45:28 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println(SquareOf(2));
+            
+    }public static int SquareOf (int n){
+
+        int x = n * n;
+        return x;
+        
+    }
+}
